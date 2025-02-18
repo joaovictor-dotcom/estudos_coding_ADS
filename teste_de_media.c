b1debf819a8c857b77ee9aa84b63744177cc8bea
joaovictor-dotcom <joaovictornyse@gmail.com>
2025-02-18 15:46:36 -0300

    teste de media e correção do super trunfo

--- teste_de_media.c
+++ teste_de_media.c
@@ -10,5 +10,13 @@ int main() {
     scanf("%f", &nota3);
     float media = (nota1 + nota2 + nota3) / 3;
     printf("A media das notas e: %.2f\n", media);
+
+    //teste de casting
+
+    int a = 5;
+    int b = 2;
+    float quociente = (float) a / b;
+
+    printf("O quociente de %d por %d e: %.2f\n", a, b, quociente);
 return 0;
 }
