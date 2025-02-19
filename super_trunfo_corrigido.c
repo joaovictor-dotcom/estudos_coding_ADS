a2885f52a5189f9e42f59fd6fb14f874228e1d61
joaovictor-dotcom <joaovictornyse@gmail.com>
2025-02-19 10:59:05 -0300

    desafio 2

--- super_trunfo_corrigido.c
+++ super_trunfo_corrigido.c
@@ -10,67 +10,81 @@ int main() {
     int populacao, turismo;
     float tamanho, pib;
 
-    printf("Voc√™ est√° come√ßando a jogar Super Trunfo!\n Inicie nos informando seu nome:\n");
+    printf("VocÍ est· comeÁando a jogar Super Trunfo!\n Inicie nos informando seu nome:\n");
     scanf("%s", nome);
     printf("Bem-vindo ao jogo, %s!\n", nome);
-    printf("Agora, qual √© o Estado da sua carta?\n");
+    printf("Agora, qual È o Estado da sua carta?\n");
     scanf("%s", estado);
 
-    printf("Agora, qual √© o c√≥digo da sua carta?\n");
+    printf("Agora, qual È o cÛdigo da sua carta?\n");
     scanf("%s", codigo);
 
-    printf("Agora, qual √© o nome da sua carta?\n");
+    printf("Agora, qual È o nome da sua carta?\n");
     scanf("%s", cidade);
 
-    printf("Agora, qual √© a popula√ß√£o da sua carta?\n");
+    printf("Agora, qual È a populaÁ„o da sua carta?\n");
     scanf("%d", &populacao);
 
-    printf("Agora, qual √© o tamanho da cidade da sua carta em km¬≤?\n");
+    printf("Agora, qual È o tamanho da cidade da sua carta em km≤?\n");
     scanf("%f", &tamanho);
 
-    printf("Agora, qual √© o PIB da cidade da sua carta?\n");
+    printf("Agora, qual È o PIB da cidade da sua carta?\n");
     scanf("%f", &pib);
 
-    printf("Agora, quantos pontos tur√≠sticos tem a cidade da sua carta?\n");
+    printf("Agora, quantos pontos turÌsticos tem a cidade da sua carta?\n");
     scanf("%d", &turismo);
 
     // segunda carta
-    printf("Agora que voc√™ j√° criou sua primeira carta, vamos criar a segunda!\n");
+    printf("Agora que vocÍ j· criou sua primeira carta, vamos criar a segunda!\n");
 
     char estado2[50], codigo2[50], cidade2[50];
     int populacao2, turismo2;
     float tamanho2, pib2;
 
     printf("Concentre-se na segunda carta agora, %s!\n", nome);
-    printf("Agora, qual √© o Estado da sua carta?\n");
+    printf("Agora, qual È o Estado da sua carta?\n");
     scanf("%s", estado2);
 
-    printf("Agora, qual √© o c√≥digo da sua carta?\n");
+    printf("Agora, qual È o cÛdigo da sua carta?\n");
     scanf("%s", codigo2);
 
-    printf("Agora, qual √© o nome da sua carta?\n");
+    printf("Agora, qual È o nome da sua carta?\n");
     scanf("%s", cidade2);
 
-    printf("Agora, qual √© a popula√ß√£o da sua carta?\n");
+    printf("Agora, qual È a populaÁ„o da sua carta?\n");
     scanf("%d", &populacao2);
 
-    printf("Agora, qual √© o tamanho da cidade da sua carta em km¬≤?\n");
+    printf("Agora, qual È o tamanho da cidade da sua carta em km≤?\n");
     scanf("%f", &tamanho2);
 
-    printf("Agora, qual √© o PIB da cidade da sua carta?\n");
+    printf("Agora, qual È o PIB da cidade da sua carta?\n");
     scanf("%f", &pib2);
 
-    printf("Agora, quantos pontos tur√≠sticos tem a cidade da sua carta?\n");
+    printf("Agora, quantos pontos turÌsticos tem a cidade da sua carta?\n");
     scanf("%d", &turismo2);
     
-    // impress√£o das cartas
-    printf("Agora eu vou mostrar os dados das cartas que voc√™ montou, %s!\n", nome);
+    // fim da entrada de dados
+    //desafio 2 - aventureiro
+
+    //c·lculo densidade populacional
+    //densidade = populacao / tamanho;
+    float densidade = (float) populacao / tamanho;
+    float densidade2 = (float) populacao2 / tamanho2;
+
+    //c·lculo pib per capta
+    //pib_per_capta = pib / populacao;
+    float pib_per_capta = pib / populacao;
+    float pib_per_capta2 = pib2 / populacao2;
+
+    // impress„o das cartas
+
+    printf("Agora eu vou mostrar os dados das cartas que vocÍ montou, %s!\n", nome);
 
     // carta 1
-    printf("Carta 1:\nEstado: %s\nC√≥digo: %s\nNome da Cidade: %s\nPopula√ß√£o: %d\n√Årea: %.2f km¬≤\nPIB: %.2f\nPontos Tur√≠sticos: %d\n", estado, codigo, cidade, populacao, tamanho, pib, turismo);
+    printf("Carta 1:\nEstado: %s\nCÛdigo: %s\nNome da Cidade: %s\nPopulaÁ„o: %d\n¡rea: %.2f km≤\nPIB: %.2f\nPontos TurÌsticos: %d\nDensidade Populacional: %f\nPIB per Capta: %f\n", estado, codigo, cidade, populacao, tamanho, pib, turismo, densidade, pib_per_capta);
 
     // carta 2
-    printf("Carta 2:\nEstado: %s\nC√≥digo: %s\nNome da Cidade: %s\nPopula√ß√£o: %d\n√Årea: %.2f km¬≤\nPIB: %.2f\nPontos Tur√≠sticos: %d\n", estado2, codigo2, cidade2, populacao2, tamanho2, pib2, turismo2);
+    printf("Carta 2:\nEstado: %s\nCÛdigo: %s\nNome da Cidade: %s\nPopulaÁ„o: %d\n¡rea: %.2f km≤\nPIB: %.2f\nPontos TurÌsticos: %d\nDensidade Populacional: %f\nPIB per Capta: %f\n", estado2, codigo2, cidade2, populacao2, tamanho2, pib2, turismo2, densidade2, pib_per_capta2);
 
     return 0;
 }
